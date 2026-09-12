c8762f60f22e6b9c39f7e5915ee53bd1a2f10318
shadracka078-sys <shadracka078@gmail.com>
2026-09-12 00:20:28 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    // Badilisha kutoka 1.9.22 kuwa 1.9.23
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.android.application") version "8.4.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
