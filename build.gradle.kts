384c3205599a41716c1fc8a2e535a90d898e8dac
shadracka078-sys <shadracka078@gmail.com>
2026-09-11 23:00:00 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,5 @@
-// build.gradle.kts (Root Level - Nje Kabisa)
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.1")
-    }
-}
-
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
